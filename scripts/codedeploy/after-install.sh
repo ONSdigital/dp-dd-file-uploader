14f31831e04b9fc56790c990d6e829451dab4c06
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-01-05 10:10:44 +0000

    Fix typo in variable name

--- scripts/codedeploy/after-install.sh
+++ scripts/codedeploy/after-install.sh
@@ -4,4 +4,4 @@ AWS_REGION=
 ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
-$(aws ecr get-login --region $AWS_REGION) && docker pull $ECR_REPOSITORY_URI/dp-dd-file-uploader:$GIT_COMMItT
+$(aws ecr get-login --region $AWS_REGION) && docker pull $ECR_REPOSITORY_URI/dp-dd-file-uploader:$GIT_COMMIT
